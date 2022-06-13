e8c5a0883dcf746092b161e26b6f8cefba850d37
Claudio Hirakawa <claudio.hirakawa@inteegra.com.br>
2022-06-13 17:52:11 -0300

    feat: prime factors with vectors

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,29 @@ fn main() {
   let purported_cc = String::from("4532095035598175");
   println!("purported_cc: {}", purported_cc);
   let result = module::check_luhn(&purported_cc);
-  println!("result: {}", result);
+  println!(
+    "{}",
+    match result {
+      true => "👍",
+      false => "👎",
+    }
+  );
+
   let purported_cpf = String::from("84546258798");
   println!("purported_cpf: {}", purported_cpf);
   let result = module::check_cpf(&purported_cpf);
-  println!("result: {}", result);
-
-  let mut natural_number = 60;
-  let mut prime_factor = 2;
-
-  loop {
-    if natural_number % prime_factor == 0 {
-      println!("{}", prime_factor);
-      natural_number = natural_number / prime_factor;
-    } else {
-      prime_factor += 1;
+  println!(
+    "{}",
+    match result {
+      true => "👍",
+      false => "👎",
     }
-    if natural_number == 1 {
-      break;
-    }
+  );
+
+  let natural_number = 63;
+  println!("natural_number: {}", natural_number);
+  let prime_factors = module::prime_factors(natural_number);  
+  for prime_factor in prime_factors {
+    print!("{} ", prime_factor);
   }
 }
--- src/module.rs
+++ src/module.rs
@@ -23,8 +23,8 @@ pub fn check_cpf(purported_cpf: &str) -> bool {
 
   for i in 0..9 {
     let digit = purported_cpf[i..i + 1].parse::<i32>().unwrap();
-    sum_first_digit = sum_first_digit + digit * mult_first_digit;
-    sum_second_digit = sum_second_digit + digit * mult_second_digit;
+    sum_first_digit += digit * mult_first_digit;
+    sum_second_digit += digit * mult_second_digit;
     mult_first_digit -= 1;
     mult_second_digit -= 1;
   }
@@ -42,4 +42,22 @@ pub fn check_cpf(purported_cpf: &str) -> bool {
     11 - (sum_second_digit % 11)
   };
   purported_first_digit == first_digit && purported_second_digit == second_digit
+}
+
+pub fn prime_factors(natural_number: i32) -> Vec<i32> {
+  let mut prime_factors: Vec<i32> = Vec::new();
+  let mut prime_factor: i32 = 2;
+  let mut natural_number: i32 = natural_number;
+  loop {
+    if natural_number % prime_factor == 0 {
+      natural_number = natural_number / prime_factor;
+      prime_factors.push(prime_factor);
+    } else {
+      prime_factor += 1;
+    }
+    if natural_number == 1 {
+      break;
+    }
+  }
+  prime_factors
 }
